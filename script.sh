e52e27e59051f4658131d0a78f52762c11f63f40
dsw49516 <dsw49516@student.dsw.edu.pl>
2023-06-24 12:39:12 +0200

    Dodano plik script.sh
    Bazowa funkcjonalność i tworzenie logów

---
+++ script.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+show_date() {
+    current_date=$(date +%Y-%m-%d)
+    echo "Dzisiejsza data: $current_date"
+}
+
+show_help() {
+    echo "Dostępne opcje:"
+    echo "  --date               Wyświetla dzisiejszą datę"
+    echo "  --logs               Tworzy 100 plików logx.txt, zawierających nazwę pliku, nazwę skryptu i datę"
+    echo "  --logs <liczba>      Tworzy określoną liczbę plików logx.txt, zawierających nazwę pliku, nazwę skryptu i datę"
+    echo "  --help               Wyświetla tę wiadomość"
+}
+
+create_logs() {
+    local count=${1:-100}
+    mkdir "logs"
+    cd logs
+    for ((i = 1; i <= count; i++)); do
+        filename="log${i}_$(basename "$0")_$(date +%Y-%m-%d).txt"
+        echo "Nazwa pliku: $filename" > "$filename"
+        echo "Nazwa skryptu: $0" >> "$filename"
+        echo "Data: $(date)" >> "$filename"
+    done
+    cd ..
+}
+
+case "$1" in
+    --date)
+        show_date
+        ;;
+    --logs)
+        create_logs "$2"
+        ;;
+    --help)
+        show_help
+        ;;
+    *)
+        echo "Nieznana opcja. Użyj --help, aby wyświetlić dostępne opcje."
+        ;;
+esac
